43d7bf2d526747f5a1110c583a2f1d341c3dffe6
Molmel <72975576+Molmel@users.noreply.github.com>
2021-06-24 11:53:57 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="pull-right hidden-xs">
+    <b></b>
+</div>
+<strong>Copyright &copy; <?php echo date('Y'); ?> <a target='_BLANK' href="http://www.mycoding.net">- SIAKAD (Sistem Informasi Akademik)</a>.</strong> All rights reserved.
